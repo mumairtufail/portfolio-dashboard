4f3804b43be84a002877acb21f71a88391b57ade
Muhammad Abdullah <abdullahorg15@gmail.com>
2024-09-30 15:35:40 +0500

    spaces in line 17 app.blade.php

--- resources/views/admin/partials/app.blade.php
+++ resources/views/admin/partials/app.blade.php
@@ -14,7 +14,7 @@
                         </a>
                         <button class="hamburger hamburger--slider" type="button">
                             <span class="hamburger-box">
-                                <span class="hamburger-inner"></span>
+                                <span class="hamburger-inner">  </span>
                             </span>
                         </button>
                     </div>
